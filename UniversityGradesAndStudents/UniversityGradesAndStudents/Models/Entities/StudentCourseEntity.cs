c47da4e1129990e8657a0b2f2c930c1af2ed483f
rohankhadgi <rohan.khadgi24@gmail.com>
2021-09-16 22:30:04 -0500

    Uni-001: Create DB Conext & Necessary Entities for the portal

---
+++ UniversityGradesAndStudents/UniversityGradesAndStudents/Models/Entities/StudentCourseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityGradesAndStudents.Models.Entities
+{
+    public class StudentCourseEntity
+    {
+        public int StudentID { get; set; }
+        public int CourseID { get; set; }
+        public StudentEntity Student { get; set; }
+        public CourseEntity Course { get; set; }
+    }
+}
---
+++ UniversityGradesAndStudents/UniversityGradesAndStudents/Models/Entities/StudentEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UniversityGradesAndStudents.Models.Entities
+{
+    public class StudentEntity
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public ICollection<CourseEntity> StudentCourses { get; set; }
+    }
+}
